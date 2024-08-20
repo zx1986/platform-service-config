d7df5c6a894caeebdb015fa197a8cfad2db7dad0
張旭 <zx1986@gmail.com>
2024-08-20 22:04:17 +0800

    chore: try to create vault secret

--- app/services/vault_service.py
+++ app/services/vault_service.py
@@ -3,7 +3,12 @@ import hvac
 # Create a client to interact with Vault
 client = hvac.Client(
     url='http://127.0.0.1:8200',
-    token='s.YOUR_VAULT_TOKEN'
+    token=''
+)
+
+client.secrets.kv.v2.configure(
+    max_versions=20,
+    mount_point='kv',
 )
 
 # Check if the client is authenticated
@@ -12,19 +17,19 @@ if client.is_authenticated():
 
     # Write a secret
     client.secrets.kv.v2.create_or_update_secret(
-        path='secret/data/my-secret',
-        secret={'username': 'my-username', 'password': 'my-password'}
+        path='hello',
+        secret={'username': 'foo', 'password': 'bar'}
     )
     print("Secret written to Vault")
 
     # Read the secret back
-    read_secret_result = client.secrets.kv.v2.read_secret_version(path='secret/data/my-secret')
+    read_secret_result = client.secrets.kv.v2.read_secret_version(path='hello')
     secret_data = read_secret_result['data']['data']
     print(f"Username: {secret_data['username']}")
     print(f"Password: {secret_data['password']}")
 
     # Delete the secret
-    client.secrets.kv.v2.delete_metadata_and_all_versions(path='secret/data/my-secret')
+    client.secrets.kv.v2.delete_metadata_and_all_versions(path='hello')
     print("Secret deleted from Vault")
 
 else:
